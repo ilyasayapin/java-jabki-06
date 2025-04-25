61c72862cd08c59f23a252dbc0366b3a319860c1
ilyasayapin <kite@nxt.ru>
2025-04-25 14:14:39 +0300

    Lesson 6. Homework

--- src/Wallet.java
+++ src/Wallet.java
@@ -2,4 +2,9 @@ public class Wallet {
 
     String owner = "Илья";
     double money;
+
+    public void displayInfo() {
+        System.out.println("Владелец кошелька: " + owner);
+        System.out.println("Баланс кошелька: " + money);
+    }
 }
