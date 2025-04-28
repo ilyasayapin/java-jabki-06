d899554a8e5a5dc63e026f0da84bced8db792d91
ilyasayapin <kite@nxt.ru>
2025-04-28 19:07:48 +0300

    Lesson 6. Homework

--- src/MainTest.java
+++ src/MainTest.java
@@ -3,27 +3,51 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
 
+//    @Test
+//    void accountTest() {
+//        Account account = new Account();
+//        Assertions.assertEquals(0.0, account.getBalance());
+//
+//        account.deposit(100);
+//        Assertions.assertEquals(100.0, account.getBalance());
+//
+//        account.deposit(80);
+//        Assertions.assertEquals(180.0, account.getBalance());
+//
+//        account.withdraw(-100);
+//        Assertions.assertEquals(180.0, account.getBalance());
+//
+//        account.withdraw(100);
+//        Assertions.assertEquals(80.0, account.getBalance());
+//
+//        account.withdraw(80);
+//        Assertions.assertEquals(0.0, account.getBalance());
+//
+//        account.withdraw(1);
+//        Assertions.assertEquals(0.0, account.getBalance());
+//    }
+
     @Test
-    void accountTest() {
-        Account account = new Account();
-        Assertions.assertEquals(0.0, account.getBalance());
+    void walletTest() {
+        Wallet wallet = new Wallet();
+        Assertions.assertEquals(0.0, wallet.getBalance());
 
-        account.deposit(100);
-        Assertions.assertEquals(100.0, account.getBalance());
+        wallet.deposite(100);
+        Assertions.assertEquals(100.0, wallet.getBalance());
 
-        account.deposit(80);
-        Assertions.assertEquals(180.0, account.getBalance());
+        wallet.deposite(-100);
+        Assertions.assertEquals(100.0, wallet.getBalance());
 
-        account.withdraw(-100);
-        Assertions.assertEquals(180.0, account.getBalance());
+        wallet.spend(50);
+        Assertions.assertEquals(50.0, wallet.getBalance());
 
-        account.withdraw(100);
-        Assertions.assertEquals(80.0, account.getBalance());
+        wallet.spend(90);
+        Assertions.assertEquals(50.0, wallet.getBalance());
 
-        account.withdraw(80);
-        Assertions.assertEquals(0.0, account.getBalance());
+        wallet.spend(-50);
+        Assertions.assertEquals(50.0, wallet.getBalance());
 
-        account.withdraw(1);
-        Assertions.assertEquals(0.0, account.getBalance());
+        wallet.spend(50);
+        Assertions.assertEquals(0.0, wallet.getBalance());
     }
 }
--- src/Wallet.java
+++ src/Wallet.java
@@ -1,34 +1,57 @@
 public class Wallet {
 
-    String owner;
-    int money;
+    private String owner = "Супермен";
+    private double ballance;
 
     Wallet() {
         this.owner = "Не известно";
-        this.money = 0;
-        }
+        this.ballance = 0;
+    }
 
     Wallet(String owner) {
         this.owner = owner;
-        this.money = 0;
-        }
+        this.ballance = 0;
+    }
 
     Wallet(int money) {
         this.owner = "Не известно";
-        this.money = money;
-        }
+        this.ballance = money;
+    }
 
     Wallet(String owner, int money) {
         this.owner = owner;
-        this.money = money;
-        }
+        this.ballance = money;
+    }
 
     public void displayInfo() {
         System.out.printf("Владелец кошелька: %s. Баланс кошелька: %s\n",
                 this.owner,
-                this.money
+                this.ballance
         );
     }
 
+    public double getBalance() {
+        return this.ballance;
+        }
+
+    public void deposite(double amount) {
+        if (amount > 0) {
+            ballance += amount;
+        } else {
+            System.out.println("Введите положительное число");
+        }
+    }
+
+    public void spend(int amount) {
+        if (amount > 0) {
+            if (this.ballance >= amount) {
+                ballance -= amount;
+            } else {
+                System.out.println("Баланс не может уйти в минус");
+            }
+        } else {
+            System.out.println("Введите положительное число");
+        }
+    }
 }
 
